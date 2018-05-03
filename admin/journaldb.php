e23c596d20fe92335fb373bd0e27e90db8a1ad8e
foresitegroup <mark@foresitegrp.com>
2018-05-03 10:39:12 -0500

    Add display option

--- admin/journaldb.php
+++ admin/journaldb.php
@@ -3,7 +3,7 @@ include("../inc/dbconfig.php");
 
 switch ($_GET['a']) {
   case "add":
-    $query = "INSERT INTO journals (titleid, volume, number, description, file1) VALUES ('" . $_POST['titleid'] . "', '" . $_POST['volume'] . "', '" . $_POST['number'] . "', '" . htmlspecialchars($_POST['description'], ENT_QUOTES) . "', '" . $_POST['file1'] . "', '" . $_POST['display'] . "')";
+    $query = "INSERT INTO journals (titleid, volume, number, description, file1, display) VALUES ('" . $_POST['titleid'] . "', '" . $_POST['volume'] . "', '" . $_POST['number'] . "', '" . htmlspecialchars($_POST['description'], ENT_QUOTES) . "', '" . $_POST['file1'] . "', '" . $_POST['display'] . "')";
     break;
   case "edit":
     $query = "UPDATE journals SET titleid = '" . $_POST['titleid'] . "', volume = '" . $_POST['volume'] . "', number = '" . $_POST['number'] . "', description = '" . htmlspecialchars($_POST['description'], ENT_QUOTES) . "', file1 = '" . $_POST['file1'] . "', display = '" . $_POST['display'] . "' WHERE id = '" . $_POST['id'] . "'";
