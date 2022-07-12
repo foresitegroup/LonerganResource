8b3a474f35d858e25f71dd1a370ef51d46559097
foresitegroup <mark@foresitegrp.com>
2022-07-12 09:56:28 -0500

    Add new analytics

--- header.php
+++ header.php
@@ -34,19 +34,18 @@
     });
   </script>
   
-  <!-- BEGIN Google Analytics -->
+  <!-- BEGIN Google Analytics - This will stop working July 1, 2023 -->
   <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
     ga('create', 'UA-31808906-1', 'auto');
     ga('send', 'pageview');
   </script>
   <!-- END Google Analytics -->
 
-  <!-- Global site tag (gtag.js) - Google Analytics (Greg) -->
+  <!-- Global site tag (gtag.js) - Google Analytics (Greg) - This will stop working July 1, 2023 -->
   <script async src="https://www.googletagmanager.com/gtag/js?id=UA-159303635-1"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
@@ -55,13 +54,22 @@
     gtag('config', 'UA-159303635-1');
   </script>
 
-  <!-- Google Tag Manager -->
+  <!-- Google Tag Manager - This will stop working July 1, 2023 -->
   <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
   })(window,document,'script','dataLayer','GTM-NPTG44D');</script>
   <!-- End Google Tag Manager -->
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-PVWD4BMHXL"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-PVWD4BMHXL');
+  </script>
 </head>
 <body onLoad="borderheight()"<?php if (!isset($PageTitle)) { echo " id=\"main\""; } ?>>
 
